623223bb4fc3b066befdbe38edf9d4edd515ddc6
Fathima Fahmidha <firozefahmidha@gmail.com>
2026-03-14 12:31:22 +0530

    return book added

--- src/Pages/AdminDashboard.jsx
+++ src/Pages/AdminDashboard.jsx
@@ -1,10 +1,35 @@
 import { Link, useNavigate } from "react-router-dom";
 import "../Styles/AdminDashboard.css";
 import "../Styles/Addbooks.css";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../Firebase";
 
 const AdminDashboard = () => {
     const navigate = useNavigate()
-    
+    const[bookCount,setBookCount]=useState(0)
+    const[studentCount,setStudentCount]=useState(0)
+    const[issuedCount,setIssuedCount]=useState(0)
+    useEffect(()=>{
+        fetchCounts()
+    })
+    const fetchCounts = async () => {
+
+        const bookSnap = await getDocs(collection(db,"Book"))
+        setBookCount(bookSnap.size)
+
+        const studentSnap = await getDocs(collection(db,"Registration"))
+        setStudentCount(studentSnap.size)
+
+        const issuedSnap = await getDocs(collection(db,"BorrowedBooks"))
+
+        const issued = issuedSnap.docs
+        .map(doc => doc.data())
+        .filter(book => book.status === "Issued")
+
+        setIssuedCount(issued.length)
+
+        }
     
     const logout = () => (
         navigate('/')
@@ -29,30 +54,25 @@ const AdminDashboard = () => {
             <img src="https://cdn-icons-png.flaticon.com/512/29/29302.png" alt="books"/>
             <div>
                 <h3>Total Books</h3>
-                <p>5000+</p>
+                <p>{bookCount}</p>
             </div>
         </div>
         <div className="dash-card">
             <img src="https://cdn-icons-png.flaticon.com/512/1946/1946429.png" alt="students"/>
             <div>
                 <h3>Total Students</h3>
-                <p>1200+</p>
+                <p>{studentCount}</p>
             </div>
         </div>
         <div className="dash-card">
             <img src="https://cdn-icons-png.flaticon.com/512/3135/3135755.png" alt="issued"/>
             <div>
                 <h3>Issued Books</h3>
-                <p>350+</p>
+                <p>{issuedCount}</p>
             </div>
         </div>
-        <div className="dash-card">
-            <img src="https://cdn-icons-png.flaticon.com/512/482/482631.png" alt="category"/>
-            <div>
-                <h3>Categories</h3>
-                <p>50+</p>
-            </div>
-        </div>
+       
+       
       </div>
     </div>
   )
